769ad6e0a34790ca4c4d94fd312c6772009a9616
Wiktor Ruszkiewicz <Wiktor.Ruszkiewicz@gds.ey.com>
2022-09-12 17:36:02 +0200

    party added Dices for players

--- src/components/dices/Dices.tsx
+++ src/components/dices/Dices.tsx
@@ -1,25 +1,80 @@
-import React, { useState } from "react";
-import { Button, Box, createStyles, Modal, Group, Select } from "@mantine/core";
 import * as dice from "../../services/diceService";
+
+import { Box, Button, Group, Modal, Select, createStyles } from "@mantine/core";
+import React, { useState } from "react";
+
+import { initializeApp } from "firebase/app";
 
 const useStyles = createStyles(() => ({
   container: {
     width: "100%",
     maxWidth: "376px",
   },
+  diceName: {
+    width: "25px",
+  },
+  diceInput: {
+    width: "80px",
+  },
+  group: {
+    gap: "5px",
+  },
 }));
+
+const initialState = [
+  { k100: "0" },
+  { k20: "0" },
+  { k12: "0" },
+  { k10: "0" },
+  { k8: "0" },
+  { k6: "0" },
+  { k4: "0" },
+  { k2: "0" },
+];
 
 const Dices: React.FC = () => {
   const { classes } = useStyles();
-  const [numOfDices, setNumOfDices] = useState("1" as string | null);
+  const [numOfDices, setNumOfDices] = useState('1');
+  // const [k100, setk100] = useState("1");
+  // const [k20, setk20] = useState("1");
+  // const [k12, setk12] = useState("1");
+  // const [k10, setk10] = useState("1");
+  // const [k8, setk8] = useState("1");
+  // const [k6, setk6] = useState("1");
+  // const [k4, setk4] = useState("1");
+  // const [k2, setk2] = useState("1");
+  const [dicesResults, setDicesResults] = useState([] as number[]);
   const dices = ["k100", "k20", "k12", "k10", "k8", "k6", "k4", "k2"];
 
-  const rollDices = () => {
-    let diceNUm = 0;
-    if (numOfDices !== null) diceNUm = parseInt(numOfDices);
-    const arr = Array(diceNUm);
-    arr.forEach(elem => elem.push(dice.rollK100()));
-    console.log(arr);
+  const rollDices = (k: string) => {
+    let diceNum = 0;
+    if (numOfDices !== null) diceNum = parseInt(numOfDices);
+    switch (k) {
+      case "k100":
+        setDicesResults(Array.from(Array(diceNum), () => dice.rollK100()));
+        break;
+      case "k20":
+        setDicesResults(Array.from(Array(diceNum), () => dice.rollK20()));
+        break;
+      case "k12":
+        setDicesResults(Array.from(Array(diceNum), () => dice.rollK12()));
+        break;
+      case "k10":
+        setDicesResults(Array.from(Array(diceNum), () => dice.rollK10()));
+        break;
+      case "k8":
+        setDicesResults(Array.from(Array(diceNum), () => dice.rollK8()));
+        break;
+      case "k6":
+        setDicesResults(Array.from(Array(diceNum), () => dice.rollK6()));
+        break;
+      case "k4":
+        setDicesResults(Array.from(Array(diceNum), () => dice.rollK4()));
+        break;
+      case "k2":
+        setDicesResults(Array.from(Array(diceNum), () => dice.rollK2()));
+        break;
+    }
   };
 
   return (
@@ -27,10 +82,12 @@ const Dices: React.FC = () => {
       {dices.map((dice) => (
         <Box key={dice}>
           <Group>
-            <p>{dice}</p>
+            <p className={classes.diceName}>{dice}</p>
             <Select
+              className={classes.diceInput}
               placeholder="number of dices"
               data={[
+                { value: "0", label: "0" },
                 { value: "1", label: "1" },
                 { value: "2", label: "2" },
                 { value: "3", label: "3" },
@@ -43,11 +100,42 @@ const Dices: React.FC = () => {
                 { value: "10", label: "10" },
               ]}
               value={numOfDices}
-              onChange={(k = '1') => {
-                setNumOfDices(k);
+              onChange={(k = "1") => {
+                if (k !== null) setNumOfDices(k);
+                // switch (dice) {
+                //   case "k100":
+                //     if (k !== null) setk100(k);
+                //     break;
+                //   case "k20":
+                //     if (k !== null) setk20(k);
+                //     break;
+                //   case "k12":
+                //     if (k !== null) setk12(k);
+                //     break;
+                //   case "k10":
+                //     if (k !== null) setk10(k);
+                //     break;
+                //   case "k8":
+                //     if (k !== null) setk8(k);
+                //     break;
+                //   case "k6":
+                //     if (k !== null) setk6(k);
+                //     break;
+                //   case "k4":
+                //     if (k !== null) setk4(k);
+                //     break;
+                //   case "k2":
+                //     if (k !== null) setk2(k);
+                //     break;
+                // }
               }}
             />
-            <Button onClick={rollDices}>Roll</Button>
+            <Button onClick={() => rollDices(dice)}>Roll</Button>
+            <Group className={classes.group}>
+              {dicesResults && dicesResults.length > 0
+                ? dicesResults.map((result) => <div>{result}</div>)
+                : null}
+            </Group>
           </Group>
         </Box>
       ))}
--- src/services/diceService.ts
+++ src/services/diceService.ts
@@ -3,6 +3,10 @@ const generateRandomNumberInRange = (min: number, max: number) =>
 
 export const rollK100 = () => generateRandomNumberInRange(1, 100);
 
+export const rollK20 = () => generateRandomNumberInRange(1, 20);
+
+export const rollK12 = () => generateRandomNumberInRange(1, 12);
+
 export const rollK10 = () => generateRandomNumberInRange(1, 10);
 
 export const rollK8 = () => generateRandomNumberInRange(1, 8);
@@ -10,6 +14,8 @@ export const rollK8 = () => generateRandomNumberInRange(1, 8);
 export const rollK6 = () => generateRandomNumberInRange(1, 6);
 
 export const rollK4 = () => generateRandomNumberInRange(1, 4);
+
+export const rollK2 = () => generateRandomNumberInRange(1, 2);
 
 export const rollForInitiativeForEnemy = () =>
   generateRandomNumberInRange(1, 100) + generateRandomNumberInRange(20, 40);
